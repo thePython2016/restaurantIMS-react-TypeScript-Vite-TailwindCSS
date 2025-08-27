00b2f3b7d29c3abbc13facb49595980afea2dc7c
Omari <infonet20th@gmail.com>
2025-08-27 20:24:50 +0300

    Comm

--- backend/app_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from auth_app import views
-
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('auth/google/', views.google_auth, name='google_auth'),
-    path('auth/registration/', views.registration_view, name='registration'),
-] 
