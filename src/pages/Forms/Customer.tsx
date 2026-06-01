56f034bec864aa51d1e788360c5dfcafff4498d6
Omari <infonet20th@gmail.com>
2026-06-01 15:15:31 +0300

    Customer Route Fixed

--- src/pages/Forms/Customer.tsx
+++ src/pages/Forms/Customer.tsx
@@ -90,7 +90,7 @@ const Customer: React.FC = () => {
     };
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/customer/', {
+      const response = await fetch('https://restaurant-management-system-dun-kappa.vercel.app/customer/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
